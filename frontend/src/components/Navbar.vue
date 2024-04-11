36fd621bf95fb0a1c9d3dcd819a76c334b1f6633
LimpSword <contact@limpsword.fr>
2024-04-11 19:22:52 +0200

    Navbar items, icon as link

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -13,7 +13,9 @@
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex flex-shrink-0 items-center">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+            <a href="/">
+              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+            </a>
           </div>
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
@@ -68,9 +70,9 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Home', href: '#', current: true },
+  { name: 'Solo', href: '#', current: false },
+  { name: 'Multiplayer', href: '#', current: false },
+  { name: 'Leaderboard', href: '#', current: false },
 ]
 </script>
